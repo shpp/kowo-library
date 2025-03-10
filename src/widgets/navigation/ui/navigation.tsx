f49aa9992ff4f2bbf700ed688fd3c1c12a53cd4f
AndrII Chudinovskykh <andron989@gmail.com>
2025-03-10 16:08:47 +0200

    fix Navigation

--- src/widgets/navigation/ui/navigation.tsx
+++ src/widgets/navigation/ui/navigation.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import Link from "next/link";
 import { Link as ChakraLink, List } from "@chakra-ui/react"
 
-import styles from './navigation.module.css';
-
 const NAVIGATION_LINKS = [
   {href: "/about", title: "Про Бібліотеку"},
   {href: "/how-it-works", title: "Як це працює"},
@@ -12,11 +10,11 @@ const NAVIGATION_LINKS = [
 
 export const Navigation = ({direction = 'row'}: {direction?: 'row' | 'column'}) => (
   <nav>
-    <List.Root className={styles.ul} flexDirection={direction}>
+    <List.Root flexDirection={direction} gap='12px' listStyleType='none' width='max-content' px='12px'>
       {NAVIGATION_LINKS.map(({href, title}) => (
         <List.Item key={title} flexShrink='0'>
           <ChakraLink asChild>
-            <Link href={href} className={styles.link}>
+            <Link href={href}>
               {title}
             </Link>
           </ChakraLink>
