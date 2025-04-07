63e4ad7c18ce549c48e7c7b9221a1847961580cf
AndrII Chudinovskykh <andron989@gmail.com>
2025-04-07 19:25:05 +0300

    Catalog onClick

--- src/features/mega-menu/ui/MegaMenu.tsx
+++ src/features/mega-menu/ui/MegaMenu.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { Box, Button, Collapsible, Separator, Stack, Text, Link as ChakraLink } from '@chakra-ui/react';
+import {Box, Button, Collapsible, Separator, Stack, Text, Link as ChakraLink, useDisclosure} from '@chakra-ui/react';
 
 import CatalogIcon from '@/shared/assets/icons/catalog-icon';
 import { Illustration } from '@/shared/ui/illustration';
@@ -53,59 +53,67 @@ const CATEGORIES = [
   },
 ];
 
-export const MegaMenu = () => (
-  <Collapsible.Root>
-    <Collapsible.Trigger asChild>
-      <Button colorPalette="kowo" rounded="lg" color="#FFF" fontWeight="600">
-        <CatalogIcon />
-        Каталог
-      </Button>
-    </Collapsible.Trigger>
-    <Collapsible.Content position="absolute" top="72px" zIndex="100" insetInline="0">
-      <Box bg="#FFFFFF" borderBottom="1px solid #D4D5D9">
-        <Box bgColor={'white'} gap="16px" columns="240px" maxWidth="1224px" marginX="auto" padding="40px 0">
-          <Stack gap="24px" marginBottom="24px" css={{ breakInside: 'avoid' }}>
-            <Link href="/books">
-              <Stack bg="#F7F8F8" padding="8px" borderRadius="8px">
-                <Stack direction="row" justify="space-between">
-                  <Text fontWeight="semibold">Усі книги</Text>
-                  <ArrowIcon />
-                </Stack>
-                <Text fontSize="sm">Переглянути всю бібліотеку</Text>
-              </Stack>
-            </Link>
+export const MegaMenu = () => {
+  const { open, onToggle, onClose } = useDisclosure();
+  
+  const handleLinkClick = () => {
+    onClose();
+  };
 
-            <Link href="#">
-              <Stack alignItems="start" padding="8px">
-                <Illustration src={books.src} alt={'Books'} width={137} height={136} />
-                <Stack direction="row" justify="space-between">
-                  <Text fontWeight="semibold">Зробіть добру справу!</Text>
-                  <ArrowIcon />
+  return (
+    <Collapsible.Root open={open} onToggle={onToggle} unmountOnExit>
+      <Collapsible.Trigger asChild>
+        <Button colorPalette="kowo" rounded="lg" color="#FFF" fontWeight="600" onClick={onToggle}>
+          <CatalogIcon />
+          Каталог
+        </Button>
+      </Collapsible.Trigger>
+      <Collapsible.Content position="absolute" top="72px" zIndex="100" insetInline="0">
+        <Box bg="#FFFFFF" borderBottom="1px solid #D4D5D9">
+          <Box bgColor={'white'} gap="16px" columns="240px" maxWidth="1224px" marginX="auto" padding="40px 0">
+            <Stack gap="24px" marginBottom="24px" css={{ breakInside: 'avoid' }}>
+              <Link href="/books" onClick={handleLinkClick}>
+                <Stack bg="#F7F8F8" padding="8px" borderRadius="8px">
+                  <Stack direction="row" justify="space-between">
+                    <Text fontWeight="semibold">Усі книги</Text>
+                    <ArrowIcon />
+                  </Stack>
+                  <Text fontSize="sm">Переглянути всю бібліотеку</Text>
                 </Stack>
-                <Text fontSize="sm">Залиште свою книгу в бібліотеці і вона знайде нового читача!</Text>
-              </Stack>
-              <Separator />
-            </Link>
-          </Stack>
-          {CATEGORIES.map((category) => (
-            <Stack key={category.title} marginBottom="24px" css={{ breakInside: 'avoid' }}>
-              <ChakraLink asChild>
-                <Link href="#">
-                  <Text fontWeight="semibold">{category.title}</Text>
-                </Link>
-              </ChakraLink>
-              <Separator />
-              {category.items.map((item) => (
-                <ChakraLink asChild key={item}>
-                  <Link href="#">
-                    <Text fontSize="sm">{item}</Text>
+              </Link>
+              
+              <Link href="#" onClick={handleLinkClick}>
+                <Stack alignItems="start" padding="8px">
+                  <Illustration src={books.src} alt={'Books'} width={137} height={136} />
+                  <Stack direction="row" justify="space-between">
+                    <Text fontWeight="semibold">Зробіть добру справу!</Text>
+                    <ArrowIcon />
+                  </Stack>
+                  <Text fontSize="sm">Залиште свою книгу в бібліотеці і вона знайде нового читача!</Text>
+                </Stack>
+                <Separator />
+              </Link>
+            </Stack>
+            {CATEGORIES.map((category) => (
+              <Stack key={category.title} marginBottom="24px" css={{ breakInside: 'avoid' }}>
+                <ChakraLink asChild>
+                  <Link href="#" onClick={handleLinkClick}>
+                    <Text fontWeight="semibold">{category.title}</Text>
                   </Link>
                 </ChakraLink>
-              ))}
-            </Stack>
-          ))}
+                <Separator />
+                {category.items.map((item) => (
+                  <ChakraLink asChild key={item}>
+                    <Link href="#" onClick={handleLinkClick}>
+                      <Text fontSize="sm">{item}</Text>
+                    </Link>
+                  </ChakraLink>
+                ))}
+              </Stack>
+            ))}
+          </Box>
         </Box>
-      </Box>
-    </Collapsible.Content>
-  </Collapsible.Root>
-);
+      </Collapsible.Content>
+    </Collapsible.Root>
+  )
+};
